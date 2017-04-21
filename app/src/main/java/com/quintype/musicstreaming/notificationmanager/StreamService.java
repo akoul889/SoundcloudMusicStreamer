a2fba0dac91284738cdf272f63d2457c8bb914e0
akshaykoul <akshay@quintype.com>
2017-04-21 17:38:26 +0530

    Switched to a media notification style with media controls

--- app/src/main/java/com/quintype/musicstreaming/notificationmanager/StreamService.java
+++ app/src/main/java/com/quintype/musicstreaming/notificationmanager/StreamService.java
@@ -3,24 +3,31 @@ package com.quintype.musicstreaming.notificationmanager;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.session.MediaSessionManager;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.CountDownTimer;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
+import android.os.RemoteException;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
-import android.widget.RemoteViews;
 
 import com.quintype.musicstreaming.R;
 import com.quintype.musicstreaming.models.Audio;
-import com.quintype.musicstreaming.ui.activities.MainActivity;
 
 import java.util.concurrent.TimeUnit;
+
+import timber.log.Timber;
 
 /**
  * @author Niels Masdorp (NielsMasdorp)
@@ -34,13 +41,28 @@ public class StreamService extends Service implements
     private static final int NOTIFY_ID = 1;
     private final float MAX_VOLUME = 1.0f;
 
+
+    public static final String ACTION_PLAY = "com.quintype.musicstreaming.ACTION_PLAY";
+    public static final String ACTION_PAUSE = "com.quintype.musicstreaming.ACTION_PAUSE";
+    public static final String ACTION_PREVIOUS = "com.quintype.musicstreaming.ACTION_PREVIOUS";
+    public static final String ACTION_NEXT = "com.quintype.musicstreaming.ACTION_NEXT";
+    public static final String ACTION_STOP = "com.quintype.musicstreaming.ACTION_STOP";
+
     public static final String STREAM_DONE_LOADING_INTENT = "stream_done_loading_intent";
     public static final String STREAM_DONE_LOADING_SUCCESS = "stream_done_loading_success";
     public static final String TIMER_UPDATE_INTENT = "timer_update_intent";
     public static final String TIMER_UPDATE_VALUE = "timer_update_value";
     public static final String TIMER_DONE_INTENT = "timer_done_intent";
 
-    private static final String ACTION_STOP = "action_stop";
+
+    //MediaSession
+    private MediaSessionManager mediaSessionManager;
+    private MediaSessionCompat mediaSession;
+    private MediaControllerCompat.TransportControls transportControls;
+
+    //AudioPlayer notification ID
+    private static final int NOTIFICATION_ID = 101;
+
 
     public enum State {PAUSED, STOPPED, PLAYING, PREPARING}
 
@@ -54,7 +76,11 @@ public class StreamService extends Service implements
 
     public void onCreate() {
         super.onCreate();
-
+        try {
+            initMediaSession();
+        } catch (RemoteException e) {
+            Timber.e("RemoteException", e);
+        }
         player = new MediaPlayer();
         player.setAudioStreamType(AudioManager.STREAM_MUSIC);
         player.setOnPreparedListener(this);
@@ -72,18 +98,18 @@ public class StreamService extends Service implements
 
     @Override
     public IBinder onBind(Intent intent) {
-        toBackground();
+        toBackground(true);
         return streamBinder;
     }
 
     @Override
     public void onRebind(Intent intent) {
-        toBackground();
+        toBackground(true);
         super.onRebind(intent);
     }
 
-    private void toBackground() {
-        stopForeground(true);
+    private void toBackground(boolean removeNotification) {
+        stopForeground(removeNotification);
     }
 
     @Override
@@ -105,59 +131,125 @@ public class StreamService extends Service implements
      * Run the service in the foreground
      * and show a notification
      */
-    private void toForeground() {
+//    private void toForeground() {
+//
+//
+//        //Todo fix the notification logic
+//        RemoteViews notificationView = new RemoteViews(getPackageName(),
+//                R.layout.notification);
+//
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+//                .setSmallIcon(android.R.drawable.stat_sys_headset)
+//                .setShowWhen(false)
+//                .setContent(notificationView);
+//
+//
+//        notificationView.setImageViewResource(R.id.streamIcon, R.mipmap.ic_launcher_round);
+//        notificationView.setTextViewText(R.id.titleTxt, getString(R.string.app_name));
+//        notificationView.setTextViewText(R.id.descTxt, currentStream.getTitle());
+//
+//        Intent closeIntent = new Intent(getApplicationContext(), StreamService.class);
+//        closeIntent.setAction(ACTION_STOP);
+//        PendingIntent pendingCloseIntent = PendingIntent.getService(getApplicationContext(), 1,
+//                closeIntent, 0);
+//
+//        notificationView.setOnClickPendingIntent(R.id.closeStream, pendingCloseIntent);
+//
+//        Intent resultIntent = new Intent(this, MainActivity.class);
+//
+//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//        stackBuilder.addParentStack(MainActivity.class);
+//        stackBuilder.addNextIntent(resultIntent);
+//        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent
+//                .FLAG_ONE_SHOT);
+//        builder.setContentIntent(resultPendingIntent);
+//
+//        Notification notification = builder.build();
+//        startForeground(NOTIFY_ID, notification);
+//
+//
+//    }
+    public void toForeground() {
 
+        int notificationAction = android.R.drawable.ic_media_pause;//needs to be initialized
+        PendingIntent play_pauseAction = null;
 
-        //Todo fix the notification logic
-        RemoteViews notificationView = new RemoteViews(getPackageName(),
-                R.layout.notification);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(android.R.drawable.stat_sys_headset)
-                .setShowWhen(false)
-                .setContent(notificationView);
-
-        notificationView.setImageViewResource(R.id.streamIcon, R.mipmap.ic_launcher_round);
-        notificationView.setTextViewText(R.id.titleTxt, getString(R.string.app_name));
-        notificationView.setTextViewText(R.id.descTxt, currentStream.getTitle());
-
+        //Build a new notification according to the current state of the MediaPlayer
+        if (state == State.PLAYING) {
+            notificationAction = android.R.drawable.ic_media_pause;
+            //create the pause action
+            play_pauseAction = playbackAction(1);
+        } else if (state == State.PAUSED) {
+            notificationAction = android.R.drawable.ic_media_play;
+            //create the play action
+            play_pauseAction = playbackAction(0);
+        }
         Intent closeIntent = new Intent(getApplicationContext(), StreamService.class);
         closeIntent.setAction(ACTION_STOP);
         PendingIntent pendingCloseIntent = PendingIntent.getService(getApplicationContext(), 1,
                 closeIntent, 0);
 
-        notificationView.setOnClickPendingIntent(R.id.closeStream, pendingCloseIntent);
 
-        Intent resultIntent = new Intent(this, MainActivity.class);
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
+                R.mipmap.ic_launcher); //replace with your own image
 
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addParentStack(MainActivity.class);
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent
-                .FLAG_ONE_SHOT);
-        builder.setContentIntent(resultPendingIntent);
+        // Create a new Notification
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new
+                NotificationCompat.Builder(this)
+                .setShowWhen(false)
+                // Set the Notification style
+                .setStyle(new NotificationCompat.MediaStyle()
+                        // Attach our MediaSession token
+                        .setMediaSession(mediaSession.getSessionToken())
+                        // Show our playback controls in the compact notification view.
+                        .setShowActionsInCompactView(0, 1, 2)
+                        .setShowCancelButton(true)
+                        .setCancelButtonIntent(pendingCloseIntent))
+                // Set the Notification color
+                .setColor(getResources().getColor(R.color.colorPrimary))
+                // Set the large and small icons
+                .setLargeIcon(largeIcon)
+                .setSmallIcon(android.R.drawable.stat_sys_headset)
 
-        Notification notification = builder.build();
+                // Set Notification content information
+                .setContentText(currentStream.getArtist())
+                .setContentTitle(currentStream.getGenre())
+                .setContentInfo(currentStream.getTitle())
+                // Add playback actions
+                .addAction(android.R.drawable.ic_media_previous, "previous", playbackAction(3))
+                .addAction(notificationAction, "pause", play_pauseAction)
+                .addAction(android.R.drawable.ic_media_next, "next", playbackAction(2));
+
+//        ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).notify
+//                (NOTIFICATION_ID, notificationBuilder.build());
+        Notification notification = notificationBuilder.build();
         startForeground(NOTIFY_ID, notification);
     }
 
     /**
      * Handle intent from notification
      *
-     * @param intent intent to add pending intent to
+     * @param playbackAction intent to add pending intent to
      */
-    private void handleIntent(Intent intent) {
-        if (intent == null || intent.getAction() == null)
-            return;
+    private void handleIntent(Intent playbackAction) {
+        Timber.d("handleIncomingActions");
+        if (playbackAction == null || playbackAction.getAction() == null) return;
 
-        String action = intent.getAction();
-
-        if (action.equalsIgnoreCase(ACTION_STOP)) {
+        String actionString = playbackAction.getAction();
+        if (actionString.equalsIgnoreCase(ACTION_PLAY)) {
+            transportControls.play();
+        } else if (actionString.equalsIgnoreCase(ACTION_PAUSE)) {
+            transportControls.pause();
+        } else if (actionString.equalsIgnoreCase(ACTION_NEXT)) {
+            transportControls.skipToNext();
+        } else if (actionString.equalsIgnoreCase(ACTION_PREVIOUS)) {
+            transportControls.skipToPrevious();
+        } else if (actionString.equalsIgnoreCase(ACTION_STOP)) {
             Log.i(TAG, "handleIntent: stopping stream from notification");
 
             stopStreaming();
             stopSleepTimer();
-            toBackground();
+            toBackground(true);
             stopSelf();
         }
     }
@@ -190,7 +282,7 @@ public class StreamService extends Service implements
 //                    .getStreamUrl(), getString(R.string.soundcloud_client_id))));
             player.setLooping(true);
             player.setVolume(MAX_VOLUME, MAX_VOLUME);
-            currentStream = stream;
+            this.currentStream = stream;
         } catch (Exception e) {
             Log.e(TAG, "playStream: ", e);
         }
@@ -282,7 +374,7 @@ public class StreamService extends Service implements
                     stopStreaming();
                     stopSleepTimer();
                     timerDoneBroadcast();
-                    toBackground();
+                    toBackground(true);
                 }
 
             }.start();
@@ -361,6 +453,7 @@ public class StreamService extends Service implements
 
     /**
      * Seek up to a position on a media player
+     *
      * @param position seek position
      */
     public void seek(int position) {
@@ -372,6 +465,127 @@ public class StreamService extends Service implements
             }
             player.seekTo((int) position);
 //            notifyChange(POSITION_CHANGED);
+        }
+    }
+
+    private PendingIntent playbackAction(int actionNumber) {
+        Timber.d("playbackAction");
+        Intent playbackAction = new Intent(this, StreamService.class);
+        switch (actionNumber) {
+            case 0:
+                // Play
+                playbackAction.setAction(ACTION_PLAY);
+                return PendingIntent.getService(this, actionNumber, playbackAction, 0);
+            case 1:
+                // Pause
+                playbackAction.setAction(ACTION_PAUSE);
+                return PendingIntent.getService(this, actionNumber, playbackAction, 0);
+            case 2:
+                // Next track
+                playbackAction.setAction(ACTION_NEXT);
+                return PendingIntent.getService(this, actionNumber, playbackAction, 0);
+            case 3:
+                // Previous track
+                playbackAction.setAction(ACTION_PREVIOUS);
+                return PendingIntent.getService(this, actionNumber, playbackAction, 0);
+            default:
+                break;
+        }
+        return null;
+    }
+
+    private void initMediaSession() throws RemoteException {
+        Timber.d("initMediaSession");
+        if (mediaSessionManager != null) return; //mediaSessionManager exists
+
+        mediaSessionManager = (MediaSessionManager) getSystemService(Context.MEDIA_SESSION_SERVICE);
+        // Create a new MediaSession
+        mediaSession = new MediaSessionCompat(getApplicationContext(), "AudioPlayer");
+        //Get MediaSessions transport controls
+        transportControls = mediaSession.getController().getTransportControls();
+        //set MediaSession -> ready to receive media commands
+        mediaSession.setActive(true);
+        //indicate that the MediaSession handles transport control commands
+        // through its MediaSessionCompat.Callback.
+        mediaSession.setFlags(MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        //Set mediaSession's MetaData
+        updateMetaData();
+
+        // Attach Callback to receive MediaSession updates
+        mediaSession.setCallback(new MediaSessionCompat.Callback() {
+            // Implement callbacks
+            @Override
+            public void onPlay() {
+                Timber.d("onPlay");
+                super.onPlay();
+                resumeStream();
+                toForeground();
+            }
+
+            @Override
+            public void onPause() {
+                Timber.d("onPause");
+                super.onPause();
+                pauseStream();
+//                buildNotification(PlaybackStatus.PAUSED);
+                toForeground();
+                toBackground(false);
+            }
+
+            @Override
+            public void onSkipToNext() {
+                Timber.d("onSkipToNext");
+                super.onSkipToNext();
+//                skipToNext();
+//                updateMetaData();
+//                buildNotification(PlaybackStatus.PLAYING);
+            }
+
+            @Override
+            public void onSkipToPrevious() {
+                Timber.d("onSkipToPrevious");
+                super.onSkipToPrevious();
+//                skipToPrevious();
+//                updateMetaData();
+//                buildNotification(PlaybackStatus.PLAYING);
+            }
+
+            @Override
+            public void onStop() {
+                Timber.d("onStop");
+                super.onStop();
+
+                //TODO check when this get triggered
+                /*stopself will destroy the service which in turn will remove the notification so
+                 I don't see the point of removeNotification()
+                  */
+//                removeNotification();
+                //Stop the service
+//                stopSelf();
+            }
+
+            @Override
+            public void onSeekTo(long position) {
+                Timber.d("onSeekTo");
+                super.onSeekTo(position);
+            }
+        });
+    }
+
+    private void updateMetaData() {
+        Timber.d("updateMetaData");
+        if (currentStream != null) {
+            Bitmap albumArt = BitmapFactory.decodeResource(getResources(),
+                    R.mipmap.ic_launcher_round); //replace with medias albumArt
+            // Update the current metadata
+            mediaSession.setMetadata(new MediaMetadataCompat.Builder()
+                    .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, currentStream
+                            .getArtwork())
+                    .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, currentStream.getArtist())
+                    .putString(MediaMetadataCompat.METADATA_KEY_GENRE, currentStream.getGenre())
+                    .putString(MediaMetadataCompat.METADATA_KEY_TITLE, currentStream.getTitle())
+                    .build());
         }
     }
 }
