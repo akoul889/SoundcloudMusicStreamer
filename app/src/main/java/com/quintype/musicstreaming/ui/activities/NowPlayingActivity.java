071264fed003ca44101d46f5ee6fe539ac9beb90
akshaykoul <akshay@quintype.com>
2017-05-05 17:53:49 +0530

    Updated the UI for Now playing screen

--- app/src/main/java/com/quintype/musicstreaming/ui/activities/NowPlayingActivity.java
+++ app/src/main/java/com/quintype/musicstreaming/ui/activities/NowPlayingActivity.java
@@ -1,19 +1,27 @@
 package com.quintype.musicstreaming.ui.activities;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.quintype.musicstreaming.R;
 import com.quintype.musicstreaming.adapter.NowPlayingAdapter;
+import com.quintype.musicstreaming.models.Audio;
 import com.quintype.musicstreaming.utils.StorageUtil;
+
+import java.util.ArrayList;
 
 public class NowPlayingActivity extends AppCompatActivity {
 
     RecyclerView nowPlayingRecyclerView;
+    TextView currentSongTitle;
+    TextView currentSongArtist;
+    ImageView currentSongThumbnail;
     StorageUtil storageUtil;
     NowPlayingAdapter nowPlayingAdapter;
 
@@ -25,12 +33,19 @@ public class NowPlayingActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
         storageUtil = new StorageUtil(getApplicationContext());
+        ArrayList<Audio> nowPlaying = storageUtil.loadAudio();
+        int currentlyPlaying = storageUtil.loadAudioIndex();
+
         nowPlayingRecyclerView = (RecyclerView) findViewById(R.id.now_playing_list);
-        nowPlayingAdapter = new NowPlayingAdapter(storageUtil.loadAudio());
+        currentSongTitle = (TextView) findViewById(R.id.song_title);
+        currentSongArtist = (TextView) findViewById(R.id.song_artist);
+        currentSongThumbnail = (ImageView) findViewById(R.id.song_thumbnail);
+        nowPlayingAdapter = new NowPlayingAdapter(nowPlaying);
         nowPlayingRecyclerView.setAdapter(nowPlayingAdapter);
+        currentSongTitle.setText(nowPlaying.get(currentlyPlaying).getTitle());
+        currentSongArtist.setText(nowPlaying.get(currentlyPlaying).getArtist());
+        Glide.with(this).load(nowPlaying.get(currentlyPlaying).getArtwork()).into(currentSongThumbnail);
     }
 
     @Override
